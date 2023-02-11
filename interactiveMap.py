2758313ac7d36370fea44218e9f2c053bbeb34c6
Dogukan Karatas <karatasdogukan@gmail.com>
2023-02-11 19:14:16 +0100

    data added

---
+++ interactiveMap.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pydeck as pdk
+import pandas as pd
+
+stationFrame = pd.read_csv('stationData.csv')
+
+layer = pdk.Layer(
+    'HexagonLayer',  
+    stationFrame,
+    get_position=['Longitude', 'Latitude'],
+    auto_highlight=True,
+    radius=7000,
+    extruded=True,
+    pickable=True,
+)
+
+# Set the viewport location
+view_state = pdk.ViewState(
+    longitude=36, latitude=40, zoom=4.5
+)
+# Combined all of it and render a viewport
+r = pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state=view_state,
+    layers=[layer]
+)
+st.pydeck_chart(r)
